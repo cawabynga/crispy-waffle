7d5377a330195fcf43e298171dc35af255d35c11
cawabynga <46137477+cawabynga@users.noreply.github.com>
2019-01-16 12:28:35 +0300

    Add files via upload

--- base.js
+++ base.js
@@ -2,42 +2,79 @@
 // 0 1 2
 // 3 4 5
 // 6 7 8
-var win = function(conditions){
-    if((conditions[0] === conditions[1]) && (conditions[1] === conditions[2]) &&  (conditions[0] !== 0)){return true}
-    if((conditions[3] === conditions[4]) && (conditions[4] === conditions[5]) &&  (conditions[3] !== 0)){return true}
-    if((conditions[6] === conditions[7]) && (conditions[7] === conditions[8]) &&  (conditions[6] !== 0)){return true}
-    if((conditions[0] === conditions[3]) && (conditions[3] === conditions[6]) &&  (conditions[0] !== 0)){return true}
-    if((conditions[1] === conditions[4]) && (conditions[4] === conditions[7]) &&  (conditions[1] !== 0)){return true}
-    if((conditions[2] === conditions[5]) && (conditions[5] === conditions[8]) &&  (conditions[2] !== 0)){return true}
-    if((conditions[0] === conditions[4]) && (conditions[4] === conditions[8]) &&  (conditions[0] !== 0)){return true}
-    if((conditions[2] === conditions[4]) && (conditions[4] === conditions[6]) &&  (conditions[2] !== 0)){return true}
+// 0 - ничего 1- крест 2- нолик
+var end_of_game = 0;
+var win = function (conditions) {
+    if ((conditions[0] === conditions[1]) && (conditions[1] === conditions[2]) && (conditions[0] !== 0)) {
+        end_of_game = 1;
+        return conditions[0]
+    } else if ((conditions[3] === conditions[4]) && (conditions[4] === conditions[5]) && (conditions[3] !== 0)) {
+        end_of_game = 1;
+        return conditions[3]
+    } else if ((conditions[6] === conditions[7]) && (conditions[7] === conditions[8]) && (conditions[6] !== 0)) {
+        end_of_game = 1;
+        return conditions[6]
+    } else if ((conditions[0] === conditions[3]) && (conditions[3] === conditions[6]) && (conditions[0] !== 0)) {
+        end_of_game = 1;
+        return conditions[0]
+    } else if ((conditions[1] === conditions[4]) && (conditions[4] === conditions[7]) && (conditions[1] !== 0)) {
+        end_of_game = 1;
+        return conditions[1]
+    } else if ((conditions[2] === conditions[5]) && (conditions[5] === conditions[8]) && (conditions[2] !== 0)) {
+        end_of_game = 1;
+        return conditions[2]
+    } else if ((conditions[0] === conditions[4]) && (conditions[4] === conditions[8]) && (conditions[0] !== 0)) {
+        end_of_game = 1;
+        return conditions[0]
+    } else if ((conditions[2] === conditions[4]) && (conditions[4] === conditions[6]) && (conditions[2] !== 0)) {
+        end_of_game = 1;
+        return conditions[2]
+    }
+    return 0;
+};
+var draw = function (conditions) {
+    return conditions[0] !== 0 &&
+        conditions[1] !== 0 &&
+        conditions[2] !== 0 &&
+        conditions[3] !== 0 &&
+        conditions[4] !== 0 &&
+        conditions[5] !== 0 &&
+        conditions[6] !== 0 &&
+        conditions[7] !== 0 &&
+        conditions[8] !== 0 &&
+        win(conditions) === 0;
 };
 var addThumbnailClickHandler = function (thumbnail, n) {
     thumbnail.addEventListener('click', function () {
         console.log(n + ' сработал');
-        if (step === 1 && condition[n] === 0){
-            crosses[2*n].classList.remove("show-none");
-            crosses[2*n + 1].classList.remove("show-none");
+        if (step === 1 && end_of_game === 0 && condition[n] === 0) {
+            crosses[2 * n].classList.remove("show-none");
+            crosses[2 * n + 1].classList.remove("show-none");
             condition[n] = 1;
-            if (win(condition)){console.log(step + " выиграл!");
+            if (win(condition) === 1) {
+                console.log(step + " выиграл!");
                 var banner1 = document.querySelector('.banner_1');
                 banner1.classList.remove("show-none");
             }
-            step ++
-        }
-        else if (condition[n] === 0){
+            if (draw(condition)) {
+                var banner3 = document.querySelector('.banner_3');
+                banner3.classList.remove("show-none");
+                end_of_game = 1;
+            }
+            step++
+        } else if (condition[n] === 0 && end_of_game === 0) {
             nots[n].classList.remove("show-none");
             condition[n] = 2;
-            if (win(condition)){console.log(step + " выиграл!");
+            if (win(condition) === 2) {
+                console.log(step + " выиграл!");
                 var banner2 = document.querySelector('.banner_2');
                 banner2.classList.remove("show-none");
             }
-            step -= 1
+            step -= 1;
         }
     });
 };
-var condition = [0,0,0,0,0,0,0,0,0];
-// 0 - ничего 1- крест 2- нолик
+var condition = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 var step = 1;
 var thumbnails = document.querySelectorAll('td');
 var crosses = document.querySelectorAll('.part-cross');
@@ -47,3 +84,5 @@ var nots = document.querySelectorAll('.circle');
 for (var i = 0; i < thumbnails.length; i++) {
     addThumbnailClickHandler(thumbnails[i], i);
 }
+
+
---
+++ godmode.js
@@ -0,0 +1,12 @@
+var winning = function (cond, diff) {
+    if (diff === 'god') {
+        if (turn === 9 && win(cond) === 2) {
+            return 'YES'
+        } else {
+            zero = 10;
+            while (zero !== -1) {
+
+            }
+        }
+    }
+};
