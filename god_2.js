98acdd12b70b350ebb87c8076279ee11cfda7577
cawabynga <46137477+cawabynga@users.noreply.github.com>
2019-01-28 12:01:47 +0300

    Add files via upload

--- god_2.js
+++ god_2.js
@@ -1,3 +1,16 @@
+// фун.-я целочисленного деления
+var div = function (val, by) {
+    return (val - val % by) / by;
+};
 var winning = function (conditions) {
-    
+    var stroke = [0, 0, 0];
+    var string = [0, 0, 0];
+    var diagonals = [0, 0];
+    for (var i1 = 0; i1 < 3; i1++) {
+        var current_stroke = [0, 0, 0];
+        var current_string = [0, 0, 0];
+        for (var i2 = 0; i2 < 3;  i2++) {
+            
+        }
+    }
 };
